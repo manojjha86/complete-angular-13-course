91c9fedce9696ac99505790578e291f4632b8471
manojjha86 <68413139+manojjha86@users.noreply.github.com>
2022-06-30 19:09:03 +0530

    Add files via upload

---
+++ view-child/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { DemoComponent } from './demo/demo.component';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'ViewChild';
+
+  @ViewChild('dobInput') dateOfBirth: ElementRef;
+  @ViewChild('ageInput') age: ElementRef;
+  @ViewChild(DemoComponent, {static: true}) demoComp: DemoComponent;
+
+  calculateAge(){
+    let birthYear = new Date(this.dateOfBirth.nativeElement.value).getFullYear();
+    let currentYear = new Date().getFullYear();
+    let age = currentYear - birthYear;
+    this.age.nativeElement.value = age;
+    // console.log(this.dateOfBirth);
+    // console.log(this.age);
+  }
+}
---
+++ view-child/src/app/demo/demo.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-demo',
+  templateUrl: './demo.component.html',
+  styleUrls: ['./demo.component.css']
+})
+export class DemoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  sayHello(){
+    console.log('Hello from demo component class!');
+  }
+}
