b5baebbefdf6d00ead5f18e9dfb1bd047216e405
manojjha86 <68413139+manojjha86@users.noreply.github.com>
2022-06-30 19:12:29 +0530

    Add files via upload

---
+++ angular-form/src/app/app.component.ts
@@ -0,0 +1,64 @@
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'AngularForms';
+  defaultCountry = 'india';
+
+  firstname:string;
+  lastname:string;
+  email: string;
+  gen:string;
+  country: string;
+
+  defaultGender = 'Female';
+
+  gender = [
+    {id: '1', value: 'Male'},
+    {id: '2', value: 'Female'},
+    {id: '3', value: 'Other'}
+  ]
+  
+  @ViewChild('myForm') form: NgForm;
+
+  onSubmit(){
+    console.log(this.form);
+
+    this.firstname = this.form.value.personDetails.firstname;
+    this.lastname = this.form.value.personDetails.lastname;
+    this.email = this.form.value.personDetails.email;
+    this.gen = this.form.value.gender;
+    this.country = this.form.value.country;
+
+    this.form.reset();
+  }
+
+  setDefaultValues(){
+    // this.form.value.personDetails.firstname = 'John';
+    // this.form.value.personDetails.lastname = 'smith';
+    // this.form.value.personDetails.email = 'abc@example.com';
+    // this.form.setValue({
+    //   country: '',
+    //   gender: '',
+    //   hobbies: '',
+    //   personDetails: {
+    //     firstname: 'John',
+    //     lastname: 'Smith',
+    //     email: 'abc@example.com'
+    //   }
+    // })
+
+    this.form.form.patchValue({
+      personDetails: {
+         firstname: 'John',
+         lastname: 'Smith',
+         email: 'abc@example.com'
+      }
+    })
+  }
+}
